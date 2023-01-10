996c5395e115d625d14379c9ebdb3d70882defb6
Andria <andriamialy@andria-mba.lan>
2023-01-10 23:00:15 +0100

    style: code linting

--- src/walls_check_cli/__init__.py
+++ src/walls_check_cli/__init__.py
@@ -1,2 +1,2 @@
 """This is the Wall-Check-Cli source package."""
-__version__ = '0.1.0'
+__version__ = "0.1.0"
--- src/walls_check_cli/cli.py
+++ src/walls_check_cli/cli.py
@@ -5,10 +5,13 @@ import requests
 
 @click.command
 @click.option("-m", "--mall", required=True, type=int)
-@click.option("--url", required=True, envvar="URL",type=str)
-def cli(mall, url):
+@click.option("--url", required=True, envvar="URL", type=str, help="URL of the backend's API")
+@click.option("--user", required=True, type=str)
+@click.option("--password", required=True, type=str)
+def cli(mall, url, user, password):
     """This is the tool for interacting with the wall checking endpoint."""
     pass
 
-if  __name__  ==  '__main__':
-	cli()
+
+if __name__ == "__main__":
+    cli()
